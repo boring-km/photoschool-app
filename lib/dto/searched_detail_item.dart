c806c49c2e9250ee8d40651653ad8a05563f2007
boring-km <kms0644804@naver.com>
2021-06-26 16:01:22 +0900

    [master] 데이터가 전부 없을 수도 있기 때문에 일단 final 해제

--- lib/dto/searched_detail_item.dart
+++ lib/dto/searched_detail_item.dart
@@ -1,13 +1,13 @@
 class SearchedDetailItem {
-  final int apiId;
-  final String name;
-  final String type;
-  final String familyType;
-  final String habitat;
-  final String detail;
-  final String imgUrl1;
-  final String imgUrl2;
-  final String imgUrl3;
+  int apiId;
+  String name;
+  String type;
+  String familyType;
+  String habitat;
+  String detail;
+  String imgUrl1;
+  String imgUrl2;
+  String imgUrl3;
 
   SearchedDetailItem(this.apiId, this.name, this.type, this.familyType, this.habitat, this.detail, this.imgUrl1, this.imgUrl2, this.imgUrl3,);
 }
