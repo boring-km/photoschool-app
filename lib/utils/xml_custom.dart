7611a3127281403800b9ed6e194faf1714e8a0a3
boring-km <ts4840644804@gmail.com>
2021-06-28 00:51:03 +0900

    [master] xml 응답 객체 에러 처리

--- lib/utils/xml_custom.dart
+++ lib/utils/xml_custom.dart
@@ -2,14 +2,19 @@ import 'package:xml_parser/xml_parser.dart';
 
 class XMLParser {
   static List<XmlElement> parseXMLItems(String searched) {
-    var parentList = XmlDocument.from(searched)!
-        .getChild("response")!
-        .getChild("body")!
-        .getChild("items");
-    if (parentList!.getChildren("item") == null) {
+    try {
+      var parentList = XmlDocument.from(searched)!
+          .getChild("response")!
+          .getChild("body")!
+          .getChild("items");
+      if (parentList!.getChildren("item") == null) {
+        return [];
+      }
+      return parentList.getChildren("item")!;
+    } on Exception catch(error) {
+      print(error);
       return [];
     }
-    return parentList.getChildren("item")!;
   }
   
   static XmlElement parseXMLItem(String searched) {
